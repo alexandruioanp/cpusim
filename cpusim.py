509f99518d0f6a703f1df40feddce610bfe8f3fe
Alexandru Pop <alexandruioanp@yahoo.co.uk>
2017-09-28 22:11:15 +0100

    Intial commit

---
+++ cpusim.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import sys
+
+def main():
+    input_filename = sys.argv[1]
+
+    while ass_file as open(input_filename, 'r'):
+        program = ass_file.readlines()
+
+if __name__ == '__main__':
+    main()
